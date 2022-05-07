d9670c0e558b048322b1da3c93a4d971b53c5132
badjateamisha <amishabadjate175@gmail.com>
2022-05-07 10:26:53 +0530

    Json

---
+++ Json/Json/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Inventory
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+
+        public List<Inventory> GetProducts()
+        {
+            return new List<Inventory>()
+            {
+                new Inventory{Name = "Rice", Price = 20, Weight = 10},
+                new Inventory{Name = "Wheat", Price = 30, Weight = 5},
+                new Inventory{Name = "Pulses", Price = 50, Weight = 3}
+            };
+
+        }
+    }
+}
